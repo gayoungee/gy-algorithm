7ae26709071f6c4bb08f3cba7b9b8e6ba4912572
gayoungee_hehe <1503zzz@gmail.com>
2022-07-25 11:13:55 +0900

    9012

--- pythonProject/스택/9012.py
+++ pythonProject/스택/9012.py
@@ -1,24 +1,28 @@
-n = int(input())
-a = []
-for i in range(n):
-    tmp = input()
-    a.append(tmp)
-
-for x in a:
-    l = list(x)
-    stack = []
-
-    for e in l:
-        if e == ')' and len(stack) > 0:
-            stack.pop()
-        elif e == ')' and len(stack) < 0:
-            print('NO')
-            break
-        else:
-            stack.append(e)
-
-    if len(stack) > 0:
-        print('NO')
+#9012 괄호
+#stack
+def solution(s):
+    stack=[]
+    for x in s:
+        if x=='(':
+            stack.append(x)
+        elif x==')':
+            if len(stack)>0:
+                stack.pop()
+            else:
+                return 0
+    if len(stack)>0:
+        return 0
     else:
-        print("YES")
+        return 1
 
+res=[]
+t=int(input())
+for i in range(t):
+    a=list(input())
+    tmp=solution(a)
+    if tmp==1:
+        res.append("YES")
+    else:
+        res.append("NO")
+for x in res:
+    print(x)
